e9f55a22bc87fcfceab99d7d0c0525f06303d681
Miligro <milo20054@gmail.com>
2020-05-18 22:27:14 +0200

    update

--- knapsack_problem/knapsack.py
+++ knapsack_problem/knapsack.py
@@ -1,9 +1,28 @@
 VtoS = []
 knapsack = []
 n_containers = int(input("Number of containers: "))
-size = list(map(int, input("Size: ").split()))
+size = list(map(int, input("Weight: ").split()))
 value = list(map(int, input("Value: ").split()))
 capacity = int(input("Capacity: "))
+
+q = n_containers
+e = capacity
+
+size.insert(0, 0)
+value.insert(0, 0)
+Dyn = [[0 for x in range(capacity + 1)] for y in range(n_containers + 1)]
+for i in range(1, n_containers + 1):
+    for j in range(1, capacity + 1):
+        if j < size[i]:
+            Dyn[i][j] = Dyn[i-1][j]
+        else:
+            Dyn[i][j] = max(Dyn[i-1][j],Dyn[i-1][j-size[i]]+value[i])
+
+
+
+size = size[1:]
+value = value[1:]
+
 for i in range(n_containers):
     VtoS.append(value[i] / size[i])
 
@@ -23,4 +42,7 @@ while True:
             VtoS = VtoS[:index] + VtoS[index + 1:]
     else:
         break
-print("Value of knapsack:", sum(knapsack))
+print("Value of knapsack (greedy):", sum(knapsack))
+
+print("Value of knapsack (dynamic-programming):",Dyn[q][e])
+
